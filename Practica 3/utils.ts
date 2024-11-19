4d55e2bd9a40955defb906a71d67a68f4be7fc29
Albergcl <126758897+Albergcl@users.noreply.github.com>
2024-11-19 17:52:43 +0100

    Delete Practica 3 directory

--- Practica 3/utils.ts
+++
@@ -1,7 +0,0 @@
-import type { Task, TaskModel } from "./types.ts";
-
-export const fromModelToTask = (model: TaskModel): Task => ({
-    id: model._id!.toString(),
-    title: model.title,
-    completed: model.completed,
-});
